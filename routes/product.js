7910a2276384f4a41aca30015985fc0612ed62d8
Your Name <khoiledangminh@gmail.com>
2022-11-25 22:27:34 +0700

    fix hbs and res /req placement

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function (res, req, next) {
+router.get("/", function (req, res, next) {
   res.send("Respond with a resource");
 });
 
