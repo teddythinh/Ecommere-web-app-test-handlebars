ee33330941fb51121bde67a45d6e0e7bcb370dd4
Teddy <cuongthinh2002@gmail.com>
2022-11-25 22:07:17 +0700

    change way to run

--- app.js
+++ app.js
@@ -39,4 +39,6 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
+const PORT = 3000;
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}.`); });
 module.exports = app;
